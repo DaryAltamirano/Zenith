cbb0b2a2653acd98a67d3eaab6ab364e34849b31
Dary Altamirano <altamiranodary16@gmail.com>
2023-06-28 21:48:50 -0500

    event post

--- sensor_driver/urls.py
+++ sensor_driver/urls.py
@@ -1,8 +1,10 @@
 # Use include() to add paths from the catalog application
 from django.urls import path
-from sensor_driver.views import listSensor, formSensor
+from sensor_driver.views import listSensor, formSensor, postSensor, postZone
 
 urlpatterns = [
     path('list/', listSensor, name="listSensor"),
     path('form/', formSensor, name="formSensor"),
+    path('postSensor/', postSensor, name="postSensor"),
+    path('postZone/', postZone, name="postZone"),
 ]
--- sensor_driver/views.py
+++ sensor_driver/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render
+from django.http import JsonResponse
 from sensor_driver.models import Sensor, Zone
-
+from django.shortcuts import redirect
 
 def listSensor(request):
     from django.forms import models
     sensors = Sensor.objects.all()
-    return render(request, 'listSensor/list.html', {"sensors": sensors })
+    return render(request, 'listSensor/list.html', {"sensors": sensors})
 
 def formSensor(request):
     protocols = ["HTTP", "MQTT"]
     times = ["Second", "Minutes", "Hours"]
     zones = Zone.objects.all()
-    return render(request, 'formSensor/form.html', {'protocols':protocols, 'zones': zones, "times": times})
+    return render(request, 'formSensor/form.html', {'protocols': protocols, 'zones': zones, "times": times})
+
+def postZone(request):
+    if request.method == 'POST':
+        return redirect("/sensor/form/")
+
+    return JsonResponse({"method": "get"})
+
+def postSensor(request):
+    if request.method == 'POST':
+        return redirect("/sensor/list/")
+
+    return JsonResponse({"method": "get"})
